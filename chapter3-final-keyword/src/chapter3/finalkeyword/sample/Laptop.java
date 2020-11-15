1663935651b0e2af14d236cb16e4017466c4a09f
Batuhan Duzgun <batuhan.duzgun@sahibinden.com>
2020-11-15 18:48:13 +0300

    Java final keyword in variable, function, class!
    Change-Id: Ibdbd83d31a99872c7c72f3ded333a89cff4ecb0f

---
+++ chapter3-final-keyword/src/chapter3/finalkeyword/sample/Laptop.java
@@ -0,0 +1,8 @@
+package chapter3.finalkeyword.sample;
+
+// Laptop sınıfı Computer sınıfından kalıtım alamıyor.
+public class Laptop {  
+	// Computer sınıfı final olduğu için kalıtım veremez!
+	//extends Computer {
+	
+}
